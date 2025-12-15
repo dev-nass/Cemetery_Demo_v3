59a2d9e42253c214309bd88dd4edc8901ac3a6ac
dev-nass <jonasvincemaca@gmail.com>
2025-12-15 22:38:43 +0800

    fix: vue setup and missing packages

---
+++ resources/js/Pages/MapView.vue
@@ -0,0 +1,8 @@
+<template>
+    <Head>
+        <title>Map View</title>
+    </Head>
+    <div>
+        <h1>Map View</h1>
+    </div>
+</template>
---
+++ resources/js/Pages/WelcomeView.vue
@@ -0,0 +1,14 @@
+<template>
+    <Head>
+        <title>Home</title>
+    </Head>
+    <div
+        class="min-h-screen flex flex-col items-center justify-center bg-gray-100"
+    >
+        <h1 class="text-4xl font-bold mb-4">Welcome to Cemetery Demo v3</h1>
+        <p class="text-lg text-gray-700">
+            This is the welcome view of the application.
+            <Link class="text-blue-500" href="/map">Map View</Link>
+        </p>
+    </div>
+</template>
